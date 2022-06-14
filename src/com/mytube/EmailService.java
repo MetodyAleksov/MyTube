243cc6fff0cb44e7a2f8a18e8a3b39703f542b0d
Metody Aleksov <teddy57603@gmail.com>
2022-06-14 17:12:19 +0300

    updated EmailService to implement IEmailService

--- src/com/mytube/EmailService.java
+++ src/com/mytube/EmailService.java
@@ -1,6 +1,6 @@
 package com.mytube;
 
-public class EmailService {
+public class EmailService implements ICustomerNotificationService {
     public void sendEmail(User user) {
         System.out.println("Notifying " + user.getEmail() + "...");
         System.out.println("Done!\n");
